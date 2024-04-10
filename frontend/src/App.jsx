32117319277039dbd624f46af3f16f9816beaeb7
LakmiUreshaa <ureshalakmi1998@gmail.com>
2024-04-10 08:40:40 +0600

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
+import Cart from './pages/Cart/Cart';
 
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
