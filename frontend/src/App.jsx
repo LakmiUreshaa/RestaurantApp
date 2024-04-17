4f2494868249d9789e5c45a54284dde5d9260362
LakmiUreshaa <ureshalakmi1998@gmail.com>
2024-04-17 16:57:19 +0600

    food cart added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,18 @@ import Home from './pages/Home/Home';
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 import Cart from './pages/Cart/Cart';
 
+
 const App = () => {
   return (
     <div className="app">
       <Navbar />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/order" element={<PlaceOrder />} />
+          </Routes>
+       
       </BrowserRouter>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {StoreContextProvider} from './context/StoreContext.jsx';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+     <StoreContextProvider>
+     <App />
+     </StoreContextProvider>
+   
   </React.StrictMode>
 );
 
